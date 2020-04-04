0cded1dba5cb4b11ad80f6911f423609069cbee2
Te4g <rouseyrolgaetan@hotmail.com>
2020-04-04 20:32:14 +0200

    test

--- components/Scanner.vue
+++ components/Scanner.vue
@@ -12,6 +12,17 @@ export default {
       inputStream: {
         name: 'Live',
         type: 'LiveStream',
+        constraints: {
+          width: window.innerWidth,
+          height: window.innerHeight,
+          facingMode: 'environment'
+        },
+        area: {
+          top: '0%',
+          right: '0%',
+          left: '0%',
+          bottom: '0%'
+        },
         target: document.querySelector('#scanProduct')
       },
       decoder: {
